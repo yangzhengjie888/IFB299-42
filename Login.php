076a1f9b849a09215086ab9c9c8783728164406d
victor1028 <wtchong57@gmail.com>
2015-09-06 00:28:30 +1000

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,9 @@
+<form action="logincheck.php" method="post">  
+    Username:<input type="text" name="username" />  
+    <br />  
+    Password:<input type="password" name="password" />  
+    <br />  
+    <input type="submit" name="submit" value="Login" />  
+          
+    <a href="register.php">Register</a>  
+</form>  
